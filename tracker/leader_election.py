5d1813977de735655df5cc93f52861f19cfd86ab
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2024-07-10 00:28:47 -0400

    Change the leader election with bully for a task

--- tracker/leader_election.py
+++ tracker/leader_election.py
@@ -1,127 +1,106 @@
-import socket
-import threading
-import time
-import multiprocessing
+import hashlib, socket, time, multiprocessing
 
+HOST = "0.0.0.0"
 PORT = "8002"
-MCASTADDR = "224.0.0.2"
-ID = str(socket.gethostbyname(socket.gethostname()))
-
-OK = 2
-ELECTION = 1
-WINNER = 3
 
 
-def mcast_call(message, mcast_addr, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
-    s.sendto(message.encode(), (mcast_addr, port))
-    s.close()
-
-
-class BullyMulticastElector:
-
-    def __init__(self, Port, Mcast_Addr):
+class BroadcastPowElector:
+    def __init__(
+        self,
+        port=PORT,
+        base_hash=hashlib.sha256("Hello world for POW".encode()).hexdigest(),
+        difficulty=6,
+        iteration=1,
+    ):
         self.id = str(socket.gethostbyname(socket.gethostname()))
-        self.port = int(Port)
+        self.port = port
+        self.ImTheLeader = False
         self.Leader = None
-        self.mcast_adrr = Mcast_Addr
-        self.InElection = False
-        self.ImTheLeader = True
 
-    def bully(self, id, otherId):
-        return int(id.split(".")[-1]) > int(otherId.split(".")[-1])
+        self.base_hash = base_hash
+        self.difficulty = difficulty
+        self.iteration = iteration
 
-    def election_call(self):
-        p = multiprocessing.Process(
-            target=mcast_call, args=(f"{ELECTION}", self.mcast_adrr, self.port)
+    def _make_pow(self, iteration, initial_hash, difficulty, id, port):
+        print("pow started")
+        nonce = 0
+        sha = hashlib.sha256()
+        sha.update(str(initial_hash).encode() + str(nonce).encode())
+        prev_hash = sha.hexdigest()
+        while prev_hash[0:difficulty] != "0" * difficulty:
+            nonce += 1
+            sha = hashlib.sha256()
+            sha.update(str(initial_hash).encode() + str(nonce).encode())
+            prev_hash = sha.hexdigest()
+
+        print(f"{id},{iteration},{initial_hash},{nonce},{prev_hash}")
+
+        s = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_DGRAM,
         )
-        p.start()
-        print("Election Started")
-
-    def winner_call(self):
-        p = multiprocessing.Process(
-            target=mcast_call, args=(f"{WINNER}", self.mcast_adrr, self.port)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        s.sendto(
+            f"{id},{iteration},{initial_hash},{nonce},{prev_hash}".encode(),
+            (str(socket.INADDR_BROADCAST), int(port)),
         )
-        p.start()
+        time.sleep(3)
+        s.close()
 
     def loop(self):
-        t = threading.Thread(target=self.server_thread)
-        t.start()
-
-        counter = 0
-        while True:
-            if not self.Leader and not self.InElection:
-                self.election_call()
-                self.InElection = True
-
-            elif self.InElection:
-                counter += 1
-                if counter == 10:
-                    if not self.Leader and self.ImTheLeader:
-                        self.ImTheLeader = True
-                        self.Leader = self.id
-                        self.InElection = False
-                        self.winner_call()
-                    counter = 0
-                    self.InElection = False
-
-            # else:
-            #     print(f"Leader: {self.Leader}")
-
-            # print(f"{counter} waiting")
-            # print(f"Leader til now {self.Leader}")
-            time.sleep(1)
-
-    def server_thread(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            membership = socket.inet_aton(self.mcast_adrr) + socket.inet_aton("0.0.0.0")
-            s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-            s.bind(("", int(PORT)))
-
+            s.bind(("", int(self.port)))
             while True:
-                try:
-                    msg, sender = s.recvfrom(1024)
-                    if not msg:
-                        continue  # Ignorar mensajes vacíos
+                p = multiprocessing.Process(
+                    target=self._make_pow,
+                    args=(
+                        self.iteration,
+                        self.base_hash,
+                        self.difficulty,
+                        self.id,
+                        self.port,
+                    ),
+                )
+                p.start()
 
-                    newId = sender[0]
-                    msg = msg.decode("utf-8")
+                timer = 20
+                while True:
+                    msg, winner = s.recvfrom(1024)
+                    id, iteration, initial_hash, nonce, curr_hash = msg.decode().split(
+                        ","
+                    )
 
-                    if msg.isdigit():
-                        # print(f"Server_thread message: {msg}")
-                        msg = int(msg)
-                        if msg == ELECTION and not self.InElection:
-                            # print(f"Election message received from: {newId}")
+                    print(
+                        "Recieved: ",
+                        id,
+                        iteration,
+                        initial_hash,
+                        nonce,
+                        curr_hash,
+                        "from",
+                        winner,
+                    )
 
-                            if not self.InElection:
-                                self.InElection = True
-                                self.Leader = None
-                                self.election_call()
-
-                            if self.bully(self.id, newId):
-                                with socket.socket(
-                                    socket.AF_INET, socket.SOCK_DGRAM
-                                ) as s_send:
-                                    s_send.sendto(f"{OK}".encode(), (newId, self.port))
-
-                        elif msg == OK:
-                            # print(f"OK message received from: {newId}")
-                            if self.Leader and self.bully(newId, self.Leader):
-                                self.Leader = newId
-                            self.ImTheLeader = False
-
-                        elif msg == WINNER:
-                            print(f"Winner message received from: {newId}")
-                            if not self.bully(self.id, newId) and (
-                                not self.Leader or self.bully(newId, self.Leader)
-                            ):
-                                self.Leader = newId
-                                if self.Leader != self.id:
-                                    self.ImTheLeader = False
-                                self.InElection = False
-
-                except Exception as e:
-                    print(f"Error in server_thread: {e}")
+                    sha = hashlib.sha256()
+                    sha.update(str(initial_hash).encode() + str(nonce).encode())
+                    hash = sha.hexdigest()
+                    if (
+                        hash == curr_hash
+                        and hash[0 : self.difficulty] == "0" * self.difficulty
+                    ):
+                        print(f"Leader Elected: {winner}")
+                        self.Leader = winner
+                        # we have a winner
+                        if id == self.id:
+                            print("I'm the leader")
+                            self.ImTheLeader = True
+                            time.sleep(4)
+                            timer = 16
+                        # for a valid hash and a more recent iteration, we update the new iteration
+                        elif int(iteration) > self.iteration:
+                            self.iteration = int(iteration)
+                        self.iteration += 1
+                        p.terminate()
+                        p.join()
+                        time.sleep(timer)
+                        break
